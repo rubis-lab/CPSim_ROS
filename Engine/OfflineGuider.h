faec059f2055d42d8932d252354d9c06f03ba330
pshmx2007 <psh19940403@gmail.com>
2020-08-19 13:59:56 +0900

    (CU-41rbk1[complete]) implement OfflineGuider

---
+++ Engine/OfflineGuider.h
@@ -0,0 +1,31 @@
+#ifndef OFFLINEGUIDER_H__
+#define OFFLINEGUIDER_H__
+
+#include "Utils.h"
+
+/** This file is engine code of CPSim-Re engine
+ * @file OfflineGuider.h
+ * @class OfflineGuider
+ * @author Alex Noble
+ * @date 2020-08-19
+ * @brief Codes for Engine-OfflineGuider 
+*/
+
+class OfflineGuider {
+private:
+public:
+    /**
+     * Constructor & Destructor
+    */
+    OfflineGuider();
+    ~OfflineGuider();
+    /**
+     * Generate Offline Guider
+     */
+    void construct_job_precedence_graph();
+    void construct_start_job_sets(int ecu_id, std::shared_ptr<Job>& current_job);
+    void construct_finish_job_sets(int ecu_id, std::shared_ptr<Job>& current_job);
+    void construct_producer_job_sets(int ecu_id, std::shared_ptr<Job>& current_job);
+};
+
+#endif
